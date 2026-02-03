7bd47267698aa5b01be495711841b7c1eb8ab54b
Tr4mbo <diegoaaranamm@gmail.com>
2026-02-03 12:12:31 -0600

    AddBar added

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(
+      backgroundColor: Colors.transparent,
+      //icono de la izquierda
+      leading: Icon(Icons.menu, color: Colors.grey[800],),
+      //icono de  la derecha
+      actions: [
+          Padding(
+            padding: const EdgeInsets.only(right:24.0),
+            child: Icon(Icons.person, color: Colors.grey[800]),
+        ),
+        ],
+      ),
+      body: Column(
+        //texto principal
+      )
+
+
+    );
+  }
+}
